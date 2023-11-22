ae5e71de4d212cf76caa2680ff084fe08d5b3a36
cristinagiacobbe <crigiak@gmail.com>
2023-11-22 23:19:01 +0100

    Define problem and tools

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+ */
+
+//TOOLS:
+//Input tags (label, input text and save-btn)
+//event listener
+// getElementById to save input-tags values
+//set variables (number_km, passenger_age, ticket_price)
+//define conditions
+//print message(calcule result) on html page
+
+
+
+
